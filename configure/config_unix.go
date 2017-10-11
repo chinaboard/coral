d7a2146810904651816cd710eaa9d0a24ee563a7
chinaboard <chinaboard@gmail.com>
2017-10-11 10:31:21 +0800

    Fix unix build error

--- configure/config_unix.go
+++ configure/config_unix.go
@@ -3,9 +3,19 @@
 package configure
 
 import (
+	"fmt"
+	"os"
 	"path"
 )
 
 func GetFiletPath(fileName string) string {
 	return path.Join(path.Join(getUserHomeDir(), ".coral", fileName))
+}
+
+func getUserHomeDir() string {
+	home := os.Getenv("HOME")
+	if home == "" {
+		fmt.Println("HOME environment variable is empty")
+	}
+	return home
 }
